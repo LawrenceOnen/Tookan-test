df9b2f2d2988509697dc7c22d48d3c276be30fe3
Lawrence <lawrenceonen83@gmail.com>
2020-08-10 07:36:56 +0300

    refactored event to use a custom event args

--- App/VideoEncoder.cs
+++ App/VideoEncoder.cs
@@ -3,6 +3,11 @@ using System.Threading;
 
 namespace NetCore.Docker
 {
+    
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video {get; set;}
+    }
     public class VideoEncoder
     {
         //Define a delegate method/ Contract that all subcribers will implement
@@ -10,11 +15,11 @@ namespace NetCore.Docker
         //Raise the event
         //Source: current class pulishing the event
         //args: additional data to send with the evvent if any
-        public delegate void VideoEncodedHandler(object source, EventArgs args);
+       // public delegate void VideoEncodedHandler(object source, EventArgs args);
       
         //Event based on delegate
         //Event is raised when video has been encoded
-        public event VideoEncodedHandler VideoEncoded;
+        public event VideoEncodedHandler <VideoEventArgs> VideoEncoded;
 
         //Method to raise te event and notify all the subcribers
         protected virtual void OnVideoEncodded()
